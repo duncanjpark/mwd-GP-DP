96cde20df6662044ef794f0e1691e10ff8ebf289
George <gep1617@gmail.com>
2024-04-29 16:29:10 -0400

    Update workout tutorial view

--- src/Components/Main/Workouts/Workout.js
+++ src/Components/Main/Workouts/Workout.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Card, CardGroup } from 'react-bootstrap';
+import { Container, Card, CardGroup, Row, Col } from 'react-bootstrap';
+import { getAllWorkouts } from '../../../Common/Services/WorkoutService';
 
 export default function Workout() {
 
@@ -11,10 +12,24 @@ export default function Workout() {
             .catch(error => console.error("Error loading workouts:", error));
     }, []);
 
-
     return (
-        <div>
-            o
-        </div>
+        <Container className='mt-5'>
+            <Row xs={1} md={2} lg={3} className="g-4">
+                {workouts.map((workout) => (
+                    <Col key={workout.id} className="d-flex">
+                        <Card className="flex-fill d-flex flex-column">
+                            <Card.Body className="flex-grow-1">
+                                <Card.Title>{workout.name}</Card.Title>
+                                <Card.Text>Difficulty: {workout.difficulty}</Card.Text>
+                                <Card.Text>Targeted Muscles: {workout.musclesTargeted.join(', ')}</Card.Text>
+                            </Card.Body>
+                                <Card.Footer className="mt-auto">
+                                    <Card.Link href={workout.video} target="_blank">Watch Video</Card.Link>
+                                </Card.Footer>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     );
 };
